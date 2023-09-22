e778a526ffe13b15ce6822a5dc4ec40c9189795c
SpaceNoob123 <markusgame31@gmail.com>
2023-09-22 20:18:32 +0300

    Add logic for ChessServer

--- Chess.Core/UDP/UdpBase.cs
+++ Chess.Core/UDP/UdpBase.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace Chess.Core.UDP
 {
-    internal class UdpBase
+    public abstract class UdpBase
     {
+        public delegate void PacketRecieved(Packet packet);
+        public event PacketRecieved? OnPacketReceived;
+
+        protected System.Net.Sockets.UdpClient Client;
+
+        protected UdpBase()
+        {
+            Client = new System.Net.Sockets.UdpClient();
+        }
+
+        public async Task<Packet> Receive()
+        {
+            var result = await Client.ReceiveAsync();
+            Packet p = Packet.Deserialize(Encoding.ASCII.GetString(result.Buffer, 0, result.Buffer.Length));
+            p.SenderEndpoint = result.RemoteEndPoint.Address + ":" + result.RemoteEndPoint.Port;
+            OnPacketReceived?.Invoke(p);
+            return p;
+        }
     }
 }
